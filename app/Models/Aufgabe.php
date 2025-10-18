ad39f385dbf4ca1cb9a0a8e2902c3e05ea03ea3c
Der-dreckige-Dan <markziolkowski215@gmail.com>
2025-10-19 01:28:42 +0200

    Api Platform weg

--- app/Models/Aufgabe.php
+++ app/Models/Aufgabe.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use ApiPlatform\Metadata\ApiResource;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Aufgabe extends Model {
 
@@ -16,4 +16,8 @@ class Aufgabe extends Model {
     protected string $description;
 
     protected int $status;
+
+    public function users(): BelongsToMany {
+        return $this->belongsToMany(User::class);
+    }
 }
